76ed2317ac949ae963a338be998c3970a073d893
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-05-26 22:53:05 +0700

    unit test viewmodel using JUnit4

---
+++ app/src/test/java/com/example/newsapiapp/viewmodel/SourceViewModelTest.kt
@@ -0,0 +1,52 @@
+package com.example.newsapiapp.viewmodel
+
+import com.example.newsapiapp.model.article.ResponseDataSource
+import com.example.newsapiapp.network.ApiService
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Call
+
+class SourceViewModelTest{
+    lateinit var service : ApiService
+
+    @Before
+    fun setUp(){
+        service = mockk()
+    }
+
+    //Di dalam fungsi setUp(), kita menggunakan fungsi mockk() untuk membuat objek palsu (mock) dari kelas yang ingin kita uji. -> APiService
+
+    @Test
+    fun testRetriveDataSource() : Unit = runBlocking {
+        val responseRetrive = mockk<Call<ResponseDataSource>>()
+
+        //membuat objek palsu (mock) responseRetrive dari kelas Call<List<Source>>
+        //Objek palsu ini akan digunakan sebagai respons palsu dari pemanggilan service.getAllSources().
+
+        every {
+            runBlocking {
+                service.getAllSource("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI1IiwiZXhwIjoxNjk5MDg5NTI4LCJpYXQiOjE2Njc1NTM1MjgsImlzcyI6ImFtaW5pdmFuIn0.A1srn810rwLwLeoaUl1zJaoTcy5noFB8Gs10hY_cGDc")
+            }
+        } returns responseRetrive
+        val result = service.getAllSource("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI1IiwiZXhwIjoxNjk5MDg5NTI4LCJpYXQiOjE2Njc1NTM1MjgsImlzcyI6ImFtaW5pdmFuIn0.A1srn810rwLwLeoaUl1zJaoTcy5noFB8Gs10hY_cGDc")
+
+
+        //verify, kita memastikan bahwa metode service.getAllSources() benar-benar dipanggil dengan argumen yang sesuai.
+
+        verify {
+            runBlocking {
+                service.getAllSource("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI1IiwiZXhwIjoxNjk5MDg5NTI4LCJpYXQiOjE2Njc1NTM1MjgsImlzcyI6ImFtaW5pdmFuIn0.A1srn810rwLwLeoaUl1zJaoTcy5noFB8Gs10hY_cGDc")
+            }
+        }
+        assertEquals(result,responseRetrive)
+
+        //assertEquals, kita membandingkan nilai result yang diperoleh dari pemanggilan service.getAllSources()
+        // dengan objek palsu responseRetrive, untuk memastikan bahwa hasilnya sesuai dengan yang diharapkan.
+
+    }
+}
